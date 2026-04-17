36eef1c3f90b6e869abb1a3283cbe2f40a9c6788
willie <murraywillie429@gmail.com>
2026-04-17 15:42:28 -0400

    Update ContactInfo.tsx

--- components/ContactInfo.tsx
+++ components/ContactInfo.tsx
@@ -7,7 +7,7 @@ export default function ContactInfo() {
   return (
     <Card className="p-6 space-y-4">
       <h2 className="text-lg font-semibold">Contact Information</h2>
-      <Input disabled value="29 Meadowbrook - road 29 Hempstead New York 11550" placeholder="Contact Address" />
+      <Input disabled value="4427 Richard Dr, Los Angeles CA 90032" placeholder="Contact Address" />
     </Card>
   );
 }
